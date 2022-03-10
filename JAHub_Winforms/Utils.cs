9ac5678bd5eb0aea6c5f577d888fe98bb0524f22
Matthew Cole <m.b.cole300@gmail.com>
2022-03-09 22:39:26 -0500

    Created a Utils class inside of the Winform Project Added a method to held handle multiple instances of forms running

---
+++ JAHub_Winforms/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JAHub_Winforms
+{
+    public static class Utils
+    {
+        public static bool IsFormOpen(string formName)
+        {
+            var OpenForms = Application.OpenForms.Cast<Form>();
+            bool isOpen = OpenForms.Any(q => q.Name == formName);
+            return isOpen;
+        }
+    }
+}
