5f60cad83671d3bce1bad3370981d21b3c84324d
Matthew Cole <m.b.cole300@gmail.com>
2022-03-11 00:02:17 -0500

    Created Side Nav(Incomplete) and Created Method to open a form in a panel

---
+++ JAHub_Winforms/FrmShop.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace JAHub_Winforms
+{
+    partial class FrmShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.btnCart = new FontAwesome.Sharp.IconButton();
+            this.btnProducts = new FontAwesome.Sharp.IconButton();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.pnlForm = new System.Windows.Forms.Panel();
+            this.pnlNav.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.pnlNav.Controls.Add(this.iconButton2);
+            this.pnlNav.Controls.Add(this.btnCart);
+            this.pnlNav.Controls.Add(this.btnProducts);
+            this.pnlNav.Controls.Add(this.pnlLogo);
+            this.pnlNav.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlNav.Location = new System.Drawing.Point(0, 0);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(141, 497);
+            this.pnlNav.TabIndex = 0;
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.iconButton2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.iconButton2.FlatAppearance.BorderSize = 0;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.Shopware;
+            this.iconButton2.IconColor = System.Drawing.SystemColors.ButtonFace;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.IconSize = 30;
+            this.iconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton2.Location = new System.Drawing.Point(0, 217);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Padding = new System.Windows.Forms.Padding(20, 0, 30, 0);
+            this.iconButton2.Size = new System.Drawing.Size(141, 47);
+            this.iconButton2.TabIndex = 4;
+            this.iconButton2.Text = "Products";
+            this.iconButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton2.UseVisualStyleBackColor = false;
+            // 
+            // btnCart
+            // 
+            this.btnCart.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnCart.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCart.FlatAppearance.BorderSize = 0;
+            this.btnCart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCart.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCart.IconChar = FontAwesome.Sharp.IconChar.ShoppingCart;
+            this.btnCart.IconColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCart.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCart.IconSize = 30;
+            this.btnCart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCart.Location = new System.Drawing.Point(0, 170);
+            this.btnCart.Name = "btnCart";
+            this.btnCart.Padding = new System.Windows.Forms.Padding(20, 0, 30, 0);
+            this.btnCart.Size = new System.Drawing.Size(141, 47);
+            this.btnCart.TabIndex = 3;
+            this.btnCart.Text = "Cart";
+            this.btnCart.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCart.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCart.UseVisualStyleBackColor = false;
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnProducts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProducts.FlatAppearance.BorderSize = 0;
+            this.btnProducts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProducts.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnProducts.IconChar = FontAwesome.Sharp.IconChar.Shopware;
+            this.btnProducts.IconColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnProducts.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnProducts.IconSize = 30;
+            this.btnProducts.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProducts.Location = new System.Drawing.Point(0, 123);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Padding = new System.Windows.Forms.Padding(20, 0, 30, 0);
+            this.btnProducts.Size = new System.Drawing.Size(141, 47);
+            this.btnProducts.TabIndex = 1;
+            this.btnProducts.Text = "Products";
+            this.btnProducts.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProducts.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnProducts.UseVisualStyleBackColor = false;
+            this.btnProducts.Click += new System.EventHandler(this.btnProducts_Click);
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(141, 123);
+            this.pnlLogo.TabIndex = 0;
+            // 
+            // pnlForm
+            // 
+            this.pnlForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlForm.Location = new System.Drawing.Point(141, 0);
+            this.pnlForm.Name = "pnlForm";
+            this.pnlForm.Size = new System.Drawing.Size(659, 497);
+            this.pnlForm.TabIndex = 1;
+            // 
+            // FrmShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 497);
+            this.Controls.Add(this.pnlForm);
+            this.Controls.Add(this.pnlNav);
+            this.Name = "FrmShop";
+            this.Text = "FrmShop";
+            this.Load += new System.EventHandler(this.FrmShop_Load);
+            this.pnlNav.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlNav;
+        private System.Windows.Forms.Panel pnlLogo;
+        private FontAwesome.Sharp.IconButton btnProducts;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private FontAwesome.Sharp.IconButton btnCart;
+        private System.Windows.Forms.Panel pnlForm;
+    }
+}
--- JAHub_Winforms/FrmShop.cs
+++ JAHub_Winforms/FrmShop.cs
@@ -12,9 +12,40 @@ namespace JAHub_Winforms
 {
     public partial class FrmShop : Form
     {
+        private Form CurrentChildForm;
         public FrmShop()
         {
             InitializeComponent();
+            CurrentChildForm = null;
         }
+
+        // Opens Form inside of Panel
+        private void OpenChildForm(Form childForm)
+        {
+            if (CurrentChildForm != null)
+            {
+                //Opens Only one form
+                CurrentChildForm.Close();
+            }
+            CurrentChildForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            pnlForm.Controls.Add(childForm);
+            pnlForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+
+        }
+
+        private void FrmShop_Load(object sender, EventArgs e)
+        {
+            OpenChildForm(new FrmProducts());
+        }
+        private void btnProducts_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FrmProducts());
+        }
+
+        
     }
 }
