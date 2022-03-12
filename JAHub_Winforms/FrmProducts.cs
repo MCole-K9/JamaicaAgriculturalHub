5beba5a869c836af63858c71faea3ee311e6fc2f
Matthew Cole <m.b.cole300@gmail.com>
2022-03-12 13:00:16 -0500

    Modified LoadProducts method to take a list of Products with intention to pass each Product to Product User Control

--- JAHub_Winforms/FrmProducts.cs
+++ JAHub_Winforms/FrmProducts.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using JAHubLib;
 
 namespace JAHub_Winforms
 {
@@ -16,9 +17,16 @@ namespace JAHub_Winforms
         {
             InitializeComponent();
         }
-        public void LoadProducts()
+        // Create Instances of product using the data that comes from Product Class
+        public void LoadProducts(List<Product> products = null)
         {
-            Shop_Controls.ucProduct ucProduct = new Shop_Controls.ucProduct();
+            Product product = new Product
+            {
+                Name = "Yam",
+                Price = 100,
+                Image = "C:/Users/Matthew Cole/source/repos/JamaicaAgricultualHub/JAHub_Winforms/Images/yam-g91011fb33_640.jpg",
+            };
+            Shop_Controls.UcProduct ucProduct = new Shop_Controls.UcProduct(product);
             fpnlProducts.Controls.Add(ucProduct);
         }
 
