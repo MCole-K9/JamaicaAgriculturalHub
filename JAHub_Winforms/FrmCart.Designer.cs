71e1fdb0808cf3d4455455d819b883fe15322a17
Matthew Cole <m.b.cole300@gmail.com>
2022-03-12 17:03:42 -0500

    Takes Cart items from product list in parent form (FrmShop)

---
+++ JAHub_Winforms/FrmCart.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace JAHub_Winforms
+{
+    partial class FrmCart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fpnlCart = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // fpnlCart
+            // 
+            this.fpnlCart.AutoScroll = true;
+            this.fpnlCart.Location = new System.Drawing.Point(1, 56);
+            this.fpnlCart.Name = "fpnlCart";
+            this.fpnlCart.Size = new System.Drawing.Size(723, 366);
+            this.fpnlCart.TabIndex = 0;
+            // 
+            // FrmCart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(727, 450);
+            this.Controls.Add(this.fpnlCart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FrmCart";
+            this.Text = "FrmCart";
+            this.Load += new System.EventHandler(this.FrmCart_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel fpnlCart;
+    }
+}
---
+++ JAHub_Winforms/FrmCart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JAHubLib;
+
+namespace JAHub_Winforms
+{
+    public partial class FrmCart : Form
+    {
+        public FrmShop _frmShop;
+        public FrmCart()
+        {
+            InitializeComponent();
+        }
+        public FrmCart(FrmShop frmShop)
+        {
+            InitializeComponent();
+            _frmShop = frmShop;
+        }
+
+        public void LoadCart(List<Product> cart)
+        {
+            foreach (var item in cart)
+            {
+                Shop_Controls.UcCartItem ucCartItem = new Shop_Controls.UcCartItem(item, this);
+                fpnlCart.Controls.Add(ucCartItem);
+            }
+            
+        }
+
+        private void FrmCart_Load(object sender, EventArgs e)
+        {
+           LoadCart(_frmShop.Cart);
+            
+        }
+    }
+}
