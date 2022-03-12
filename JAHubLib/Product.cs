74041a5bb51a619b2c3d29330d4b2a24d9c90927
Matthew Cole <m.b.cole300@gmail.com>
2022-03-12 12:04:11 -0500

    Created Product Class

---
+++ JAHubLib/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAHubLib
+{
+    public class Product
+    {
+        private string name;
+        private string description;
+        private string category;
+        private string image;
+        private float price;
+
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public string Category { get => category; set => category = value; }
+        public string Image { get => image; set => image = value; }
+        public float Price { get => price; set => price = value; }
+    }
