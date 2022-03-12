ed02b72cfb46b7b983ad1e8eaada9cc65ad2dd93
Matthew Cole <m.b.cole300@gmail.com>
2022-03-12 12:51:42 -0500

    Populates controls with with Product Object passed in

---
+++ JAHub_Winforms/Shop_Controls/UcProduct.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace JAHub_Winforms.Shop_Controls
+{
+    partial class UcProduct
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbImage = new System.Windows.Forms.PictureBox();
+            this.lblItemName = new System.Windows.Forms.Label();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.lblPrice = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbImage
+            // 
+            this.pcbImage.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pcbImage.Location = new System.Drawing.Point(0, 0);
+            this.pcbImage.Margin = new System.Windows.Forms.Padding(4);
+            this.pcbImage.Name = "pcbImage";
+            this.pcbImage.Size = new System.Drawing.Size(210, 160);
+            this.pcbImage.TabIndex = 0;
+            this.pcbImage.TabStop = false;
+            // 
+            // lblItemName
+            // 
+            this.lblItemName.AutoSize = true;
+            this.lblItemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItemName.Location = new System.Drawing.Point(59, 174);
+            this.lblItemName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblItemName.Name = "lblItemName";
+            this.lblItemName.Size = new System.Drawing.Size(87, 20);
+            this.lblItemName.TabIndex = 1;
+            this.lblItemName.Text = "Item Name";
+            // 
+            // btnView
+            // 
+            this.btnView.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnView.Location = new System.Drawing.Point(3, 248);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(101, 35);
+            this.btnView.TabIndex = 2;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAddToCart.Location = new System.Drawing.Point(106, 248);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(101, 35);
+            this.btnAddToCart.TabIndex = 2;
+            this.btnAddToCart.Text = "Add to Cart";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(49, 209);
+            this.lblPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(112, 20);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "$0000 Per Lbs";
+            // 
+            // ucProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnAddToCart);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblItemName);
+            this.Controls.Add(this.pcbImage);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ucProduct";
+            this.Size = new System.Drawing.Size(210, 293);
+            this.Load += new System.EventHandler(this.UcProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbImage;
+        private System.Windows.Forms.Label lblItemName;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnAddToCart;
+        private System.Windows.Forms.Label lblPrice;
+    }
+}
---
+++ JAHub_Winforms/Shop_Controls/UcProduct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JAHubLib;
+
+namespace JAHub_Winforms.Shop_Controls
+{
+    public partial class UcProduct : UserControl
+    {
+        private Product product;
+        public UcProduct(Product product)
+        {
+            InitializeComponent();
+            this.product = product;
+        }
+
+        public void PopulateFields()
+        {
+            lblItemName.Text = product.Name;
+            lblPrice.Text = $"${product.Price} Per Lbs";
+            pcbImage.Image = Image.FromFile(product.Image);
+           
+        }
+
+        private void UcProduct_Load(object sender, EventArgs e)
+        {
+            PopulateFields();
+        }
+    }
+}
