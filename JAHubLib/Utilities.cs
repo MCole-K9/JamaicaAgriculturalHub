2694bcbfc234f4cbea3f9a8346ab19ac3e4cb6eb
Matthew Cole <m.b.cole300@gmail.com>
2022-03-12 23:14:18 -0500

    Created Utitlities Class inside class library and added GetFilePath() method

---
+++ JAHubLib/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JAHubLib
+{
+    public static class Utilities
+    {
+        //Returns file path from base-directory up to Project Namespace
+        public  static string GetFilePath()
+        {
+            String path = Directory.GetCurrentDirectory();
+            path = path.Substring(0, path.Length - 10);
+
+            return path;
+        }
+    }
+}
