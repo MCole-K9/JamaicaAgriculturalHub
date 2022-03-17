15f95af16236d655531788e8f93ec94324acfd03
khalalimsheriff@outlook.com <khalalimsheriff@outlook.com>
2022-03-16 20:30:38 -0500

    Begun writing "FarmerRecord" class

---
+++ JAHubLib/FarmerRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAHubLib
+{
+    public class FarmerRecord
+    {
+        String FirstName { get; set; };
+        String LastName { get; set; };
+        String MiddleName { get; set; };
+
+
+    }
+}
