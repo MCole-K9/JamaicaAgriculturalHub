ba85117b500795179521131285abbab073b05a9b
Garaine Baker <garainebaker01@gmail.com>
2022-03-20 15:45:45 -0500

    Added Security image to resourse file

--- JAHub_Winforms/Properties/Resources.Designer.cs
+++ JAHub_Winforms/Properties/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace JAHub_Winforms.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap security_settings_I_ {
+            get {
+                object obj = ResourceManager.GetObject("security settings(I)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap user__3___1_ {
             get {
                 object obj = ResourceManager.GetObject("user__3___1_", resourceCulture);
