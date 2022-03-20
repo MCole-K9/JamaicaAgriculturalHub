262ca2570b54ce99046ead8fcd6bebc15725fe2e
Shemar Robinson <srobinson19j@vtdi.edu.jm>
2022-03-20 16:27:29 -0500

    Grant Forms

---
+++ JAHub_Winforms/FrmGeneralGrantInfo.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace JAHub_Winforms
+{
+    partial class FrmGeneralGrantInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.lbllink = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblArcsLAnd = new System.Windows.Forms.Label();
+            this.txtArcesLand = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTrn = new System.Windows.Forms.Label();
+            this.txtTrn = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(324, 797);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(68, 16);
+            this.linkLabel2.TabIndex = 58;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "linkLabel2";
+            // 
+            // lbllink
+            // 
+            this.lbllink.AutoSize = true;
+            this.lbllink.Location = new System.Drawing.Point(324, 750);
+            this.lbllink.Name = "lbllink";
+            this.lbllink.Size = new System.Drawing.Size(68, 16);
+            this.lbllink.TabIndex = 57;
+            this.lbllink.TabStop = true;
+            this.lbllink.Text = "linkLabel1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(485, 561);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(336, 17);
+            this.label3.TabIndex = 56;
+            this.label3.Text = "Please select search to see available grants for you";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(498, 590);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.button1.Size = new System.Drawing.Size(309, 64);
+            this.button1.TabIndex = 55;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "20,000 - 50,000",
+            "50,000 - 75,000",
+            "75,000 - 100,000",
+            "100,000+"});
+            this.comboBox2.Location = new System.Drawing.Point(463, 372);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(508, 24);
+            this.comboBox2.TabIndex = 54;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(459, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(267, 16);
+            this.label4.TabIndex = 53;
+            this.label4.Tag = "";
+            this.label4.Text = "How many arcces of Land do you possess?";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "20,000 - 50,000",
+            "50,000 - 75,000",
+            "75,000 - 100,000",
+            "100,000+"});
+            this.comboBox1.Location = new System.Drawing.Point(463, 436);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(412, 24);
+            this.comboBox1.TabIndex = 52;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(459, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Monthly Income:\r\n";
+            // 
+            // lblArcsLAnd
+            // 
+            this.lblArcsLAnd.AutoSize = true;
+            this.lblArcsLAnd.Location = new System.Drawing.Point(459, 276);
+            this.lblArcsLAnd.Name = "lblArcsLAnd";
+            this.lblArcsLAnd.Size = new System.Drawing.Size(83, 16);
+            this.lblArcsLAnd.TabIndex = 50;
+            this.lblArcsLAnd.Tag = "";
+            this.lblArcsLAnd.Text = "Land owned:\r\n";
+            // 
+            // txtArcesLand
+            // 
+            this.txtArcesLand.Location = new System.Drawing.Point(463, 302);
+            this.txtArcesLand.Name = "txtArcesLand";
+            this.txtArcesLand.Size = new System.Drawing.Size(567, 22);
+            this.txtArcesLand.TabIndex = 49;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(459, 148);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(75, 16);
+            this.lblLastName.TabIndex = 48;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(463, 173);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(567, 22);
+            this.txtLastName.TabIndex = 43;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(442, 376);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 47;
+            // 
+            // lblTrn
+            // 
+            this.lblTrn.AutoSize = true;
+            this.lblTrn.Location = new System.Drawing.Point(459, 208);
+            this.lblTrn.Name = "lblTrn";
+            this.lblTrn.Size = new System.Drawing.Size(39, 16);
+            this.lblTrn.TabIndex = 46;
+            this.lblTrn.Text = "TRN:";
+            // 
+            // txtTrn
+            // 
+            this.txtTrn.Location = new System.Drawing.Point(463, 234);
+            this.txtTrn.Name = "txtTrn";
+            this.txtTrn.Size = new System.Drawing.Size(567, 22);
+            this.txtTrn.TabIndex = 45;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(459, 87);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(65, 16);
+            this.lblName.TabIndex = 44;
+            this.lblName.Text = "First ame:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(463, 113);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(567, 22);
+            this.txtFirstName.TabIndex = 42;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 953);
+            this.panel2.TabIndex = 41;
+            // 
+            // FrmGeneralGrantInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 953);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.lbllink);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblArcsLAnd);
+            this.Controls.Add(this.txtArcesLand);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTrn);
+            this.Controls.Add(this.txtTrn);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.panel2);
+            this.Name = "FrmGeneralGrantInfo";
+            this.Text = "FrmGeneralGrantInfo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel lbllink;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblArcsLAnd;
+        private System.Windows.Forms.TextBox txtArcesLand;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTrn;
+        private System.Windows.Forms.TextBox txtTrn;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ JAHub_Winforms/FrmGrantStatus.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace JAHub_Winforms
+{
+    partial class FrmGrantStatus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGrantStatus));
+            this.btncheck = new System.Windows.Forms.Button();
+            this.lbltrn = new System.Windows.Forms.Label();
+            this.txttrn = new System.Windows.Forms.TextBox();
+            this.rtbtrackinfo = new System.Windows.Forms.RichTextBox();
+            this.pansidebarfhc = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btncheck
+            // 
+            this.btncheck.Location = new System.Drawing.Point(476, 630);
+            this.btncheck.Name = "btncheck";
+            this.btncheck.Size = new System.Drawing.Size(75, 23);
+            this.btncheck.TabIndex = 37;
+            this.btncheck.Text = "Chaeck";
+            this.btncheck.UseVisualStyleBackColor = true;
+            // 
+            // lbltrn
+            // 
+            this.lbltrn.AutoSize = true;
+            this.lbltrn.Location = new System.Drawing.Point(230, 578);
+            this.lbltrn.Name = "lbltrn";
+            this.lbltrn.Size = new System.Drawing.Size(42, 16);
+            this.lbltrn.TabIndex = 36;
+            this.lbltrn.Text = "TRN :\r\n";
+            this.lbltrn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txttrn
+            // 
+            this.txttrn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txttrn.Location = new System.Drawing.Point(312, 578);
+            this.txttrn.Name = "txttrn";
+            this.txttrn.Size = new System.Drawing.Size(364, 22);
+            this.txttrn.TabIndex = 35;
+            this.txttrn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rtbtrackinfo
+            // 
+            this.rtbtrackinfo.BackColor = System.Drawing.SystemColors.Menu;
+            this.rtbtrackinfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbtrackinfo.Location = new System.Drawing.Point(210, 476);
+            this.rtbtrackinfo.Name = "rtbtrackinfo";
+            this.rtbtrackinfo.Size = new System.Drawing.Size(665, 96);
+            this.rtbtrackinfo.TabIndex = 34;
+            this.rtbtrackinfo.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor in" +
+    "cididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+    " exercitation";
+            // 
+            // pansidebarfhc
+            // 
+            this.pansidebarfhc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.pansidebarfhc.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pansidebarfhc.Location = new System.Drawing.Point(0, 0);
+            this.pansidebarfhc.Name = "pansidebarfhc";
+            this.pansidebarfhc.Size = new System.Drawing.Size(191, 953);
+            this.pansidebarfhc.TabIndex = 31;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(525, 334);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(8, 8);
+            this.pictureBox2.TabIndex = 33;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(189, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(899, 389);
+            this.pictureBox1.TabIndex = 32;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmGrantStatus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 953);
+            this.Controls.Add(this.btncheck);
+            this.Controls.Add(this.lbltrn);
+            this.Controls.Add(this.txttrn);
+            this.Controls.Add(this.rtbtrackinfo);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pansidebarfhc);
+            this.Name = "FrmGrantStatus";
+            this.Text = "FrmGrantStatus";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncheck;
+        private System.Windows.Forms.Label lbltrn;
+        private System.Windows.Forms.TextBox txttrn;
+        private System.Windows.Forms.RichTextBox rtbtrackinfo;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pansidebarfhc;
+    }
+}
