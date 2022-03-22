34a4238d4f50a563de397e2ced317e00bd773304
Matthew Cole <m.b.cole300@gmail.com>
2022-03-21 20:39:25 -0500

    Created Rating User Control

---
+++ JAHub_Winforms/Shop_Controls/UsrRating.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace JAHub_Winforms.Shop_Controls
+{
+    partial class UsrRating
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblRating = new System.Windows.Forms.Label();
+            this.lblComment = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(3, 9);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(82, 13);
+            this.lblCustomerName.TabIndex = 0;
+            this.lblCustomerName.Text = "Customer Name";
+            // 
+            // lblRating
+            // 
+            this.lblRating.AutoSize = true;
+            this.lblRating.Location = new System.Drawing.Point(3, 33);
+            this.lblRating.Name = "lblRating";
+            this.lblRating.Size = new System.Drawing.Size(40, 13);
+            this.lblRating.TabIndex = 0;
+            this.lblRating.Text = "0 Stars";
+            // 
+            // lblComment
+            // 
+            this.lblComment.AutoSize = true;
+            this.lblComment.Location = new System.Drawing.Point(3, 56);
+            this.lblComment.Name = "lblComment";
+            this.lblComment.Size = new System.Drawing.Size(51, 13);
+            this.lblComment.TabIndex = 0;
+            this.lblComment.Text = "Comment";
+            // 
+            // UsrRating
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.Controls.Add(this.lblComment);
+            this.Controls.Add(this.lblRating);
+            this.Controls.Add(this.lblCustomerName);
+            this.Name = "UsrRating";
+            this.Size = new System.Drawing.Size(308, 97);
+            this.Load += new System.EventHandler(this.UsrRating_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label lblRating;
+        private System.Windows.Forms.Label lblComment;
+    }
+}
