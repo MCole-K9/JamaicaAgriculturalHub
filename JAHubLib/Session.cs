788ae68b8792c82907d30f9c78867c415f2e83b9
khalalimsheriff@outlook.com <khalalimsheriff@outlook.com>
2022-03-23 01:38:07 -0500

    Created Session.cs (enum UserRole, enum PasswordResult), wrote placeholder code for Session object

---
+++ JAHubLib/Session.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAHubLib
+{
+    public enum UserRole
+    {
+        Admin = 0,
+        Customer = 1,
+        Farmer = 2,
+        GrantOfficer = 3,
+    }
+
+    public enum PasswordResult
+    {
+        NoMatchingCredentials = 0,
+        CredentialsIncorrect = 1,
+        Success = 2,
+    }
+
+    public class Session
+    {
+        /* This should basically keep space for a method that checks the details
+         * entered into the login, then if correct makes a readonly property of role and 
+         * 
+         */
+
+        UserRole _userRole;
+        int _userId;
+
+        // It's never necessary to change these upon a successful login, the object manages this
+        public UserRole UserRole => _userRole;
+        public int UserId => _userId;
+
+        // unsure whether or not i want this to return a boolean or nothing yet
+        public PasswordResult Login(int userId, String password)
+        {
+            // this is roughly where the query will go
+            
+            if (userId == 1)
+            {
+                if (password == "test")
+                {
+                    _userId = 1;
+                    _userRole = UserRole.Admin;
+                    
+                    return PasswordResult.Success;
+                }
+            }
+            else if (userId == 3)
+            {
+                return PasswordResult.CredentialsIncorrect;
+            }
+
+            // Basically:
+            // - Take the username and password
+            // - Check them against the database
+            // - if successful, fill attributes for blahblah
+            // - if unsuccessful, return false (maybe return a value indicating success/failure type?)
+
+            return PasswordResult.NoMatchingCredentials;
+        }
+    }
+}
