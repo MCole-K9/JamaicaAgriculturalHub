18d37ce42a809ee20f06f1849ef29bee90b76888
Garaine Baker <garainebaker01@gmail.com>
2022-03-25 21:16:09 -0500

    Added order and orderItem class

---
+++ JAHubLib/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAHubLib
+{
+    internal class Order
+    {
+        public String OrderId { get; set; }
+        public int OrderNumber { get; set; }
+        public float TotalAmount { get; set; }
+        //public Customer customer { get; set; }
+    }
+}
