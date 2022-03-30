461b603b147a2f074f8fa779f44b7f23c6cc12dd
khalalimsheriff@outlook.com <khalalimsheriff@outlook.com>
2022-03-30 08:05:26 -0500

    first pass of writing method for btnDeleteUser

--- JAHub_Winforms/FrmAdminContainer.cs
+++ JAHub_Winforms/FrmAdminContainer.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using JAHubLib;
 
 namespace JAHub_Winforms
 {
     public partial class FrmAdminContainer : Form
     {
+        User _user;
         public FrmAdminContainer()
         {
             InitializeComponent();
+            _user = new User();
         }
 
         private void btnSelectUser_Click(object sender, EventArgs e)
@@ -52,10 +55,31 @@ namespace JAHub_Winforms
 
         private void btnDeleteUser_Click(object sender, EventArgs e)
         {
-            // This needs to offer to delete the user
-            // ask the user to confirm this (messagebox?) using admin password
-            // if the password matches then cool
-            // else say "password incorrect" popup, raise the messagebox again
+            String message = "Are you sure you want to delete the account of" + 
+                _user.FirstName + _user.LastName;
+            const String caption = "Delete Account";
+            
+            var result = MessageBox.Show(message, caption,
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                // query to delete the corresponding record
+                // return the user to Select User
+                
+                HideUserOptions();
+
+                if (pnlFormHolder.Controls.Count > 0)
+                {
+                    pnlFormHolder.Controls.Clear();
+                }
+
+                pnlFormHolder.Controls.Add(new FrmAdminSelectUser());
+                pnlFormHolder.Controls[0].Show();
+
+                // empty the Current User table
+            }
         }
 
         private void btnCreateNewUser_Click(object sender, EventArgs e)
@@ -76,6 +100,22 @@ namespace JAHub_Winforms
                 control.Width = pnlFormHolder.Width;
                 control.Height = pnlFormHolder.Height;
             }
+        }
+
+        // Literally: hides the View Moderation Log, Edit User, and Delete User options
+        private void HideUserOptions()
+        {
+            btnDeleteUser.Visible = false;
+            btnViewModeration.Visible = false;
+            btnEditUser.Visible = false;
+        }
+
+        // And the inverse, to show the user
+        private void ShowUserOptions()
+        {
+            btnDeleteUser.Show();
+            btnViewModeration.Show();
+            btnEditUser.Show();
         }
     }
 }
