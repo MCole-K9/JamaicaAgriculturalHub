849e314db213bbc6b29f406f93aebd6a66cc9be3
Matthew Cole <m.b.cole300@gmail.com>
2022-04-03 00:32:07 -0500

    Created And Implemented DeleteImages() Method

--- JAHubLib/Utilities.cs
+++ JAHubLib/Utilities.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data.SqlClient;
 
 namespace JAHubLib
 {
@@ -17,6 +18,14 @@ namespace JAHubLib
 
             return path;
         }
+        public static int GetFilePathLength()
+        {
+            String path = Directory.GetCurrentDirectory();
+            path = path.Substring(0, path.Length - 10);
+
+            return path.Length;
+        }
+
         public static string getConnectionString()
         {
             return "Data Source=jamaicaagriculturalhub.mssql.somee.com;Initial Catalog=jamaicaagriculturalhub;Persist Security Info=True;User ID=Ethan_Hughs_SQLLogin_1;Password=yq8mavdef8";
@@ -46,6 +55,50 @@ namespace JAHubLib
             File.Copy(destination, destination + ".bak");
             File.Delete(destination);
             File.Copy(source, destination);
+        }
+
+        //Deletes Files(Images) that are not in the database
+        public static void DeleteImages()
+        {
+            string[] Directoryimages = Directory.GetFiles($"{GetFilePath()}/Images");
+            List<String> DBimages = new List<String>();
+
+            using (SqlConnection connection = new SqlConnection(getConnectionString()))
+            {
+                connection.Open();
+
+                string query = "Select (Image) FROM Product";
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+
+                SqlDataReader sqlDataReader = cmd.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    DBimages.Add($"{GetFilePath()}/Images\\{sqlDataReader["Image"].ToString()}");
+                }
+            }
+
+            for (int i = 0; i < Directoryimages.Length; i++)
+            {
+                bool isInDB = false;
+
+                foreach (var item in DBimages)
+                {
+                    if(Directoryimages[i] == item)
+                    {
+                        isInDB = true;
+                    }
+                }
+
+                if (!isInDB)
+                {
+                    File.Delete(Directoryimages[i]);
+                }
+
+            }
+            
+            
         }
     }
 }
