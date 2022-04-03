6e6597d6971472bcfec7655a75d10d3776355512
Garaine Baker <garainebaker01@gmail.com>
2022-04-03 14:52:15 -0500

    added UpdateName Form

---
+++ JAHub_Winforms/FrmUpdateName.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace JAHub_Winforms
+{
+    partial class FrmUpdateName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblNamelabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblNamelabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 82);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblNamelabel
+            // 
+            this.lblNamelabel.AutoSize = true;
+            this.lblNamelabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNamelabel.Location = new System.Drawing.Point(82, 45);
+            this.lblNamelabel.Name = "lblNamelabel";
+            this.lblNamelabel.Size = new System.Drawing.Size(62, 25);
+            this.lblNamelabel.TabIndex = 0;
+            this.lblNamelabel.Text = "Name";
+            // 
+            // FrmUpdateName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmUpdateName";
+            this.Text = "FrmUpdateName";
+            this.Load += new System.EventHandler(this.FrmUpdateName_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblNamelabel;
+    }
+}
---
+++ JAHub_Winforms/FrmUpdateName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JAHub_Winforms
+{
+    public partial class FrmUpdateName : Form
+    {
+        public FrmUpdateName()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmUpdateName_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
