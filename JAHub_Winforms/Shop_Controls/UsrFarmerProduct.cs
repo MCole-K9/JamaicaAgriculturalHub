8033af050b1f3c4e45c5d60ab74ed9c08f7ff013
Matthew Cole <m.b.cole300@gmail.com>
2022-04-03 00:46:06 -0500

    Populated fields with product data and added functionlity for updatebtn click event

--- JAHub_Winforms/Shop_Controls/UsrFarmerProduct.cs
+++ JAHub_Winforms/Shop_Controls/UsrFarmerProduct.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using JAHubLib;
 
 namespace JAHub_Winforms.Shop_Controls
 {
     public partial class UsrFarmerProduct : UserControl
     {
+        private Product _product;
+        private FrmManageProducts _frmManageProducts;
         public UsrFarmerProduct()
         {
             InitializeComponent();
+        }
+        public UsrFarmerProduct(Product product, FrmManageProducts frmManageProducts)
+        {
+            InitializeComponent();
+            _product = product;
+            _frmManageProducts = frmManageProducts;
+        }
+        public void PopulateFields()
+        {
+            lblItemName.Text = _product.Name;
+            lblPrice.Text = $"${_product.Price} Per Lbs";
+            pcbImage.Image = Image.FromFile(_product.Image);
+        }
+
+        private void UsrFarmerProduct_Load(object sender, EventArgs e)
+        {
+            PopulateFields();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            _frmManageProducts._frmProfile.OpenChildForm(new FrmAddEditProduct(_product));
         }
     }
 }
