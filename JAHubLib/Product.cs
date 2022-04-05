ab5a06340fddd45878ecdfe9abbb0e2d3e339aa4
Matthew Cole <m.b.cole300@gmail.com>
2022-04-05 17:43:00 -0500

    Implemented FetchReviews()

--- JAHubLib/Product.cs
+++ JAHubLib/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace JAHubLib
 {
@@ -11,7 +12,7 @@ namespace JAHubLib
         private int id;
         private string name;
         private string description;
-        private string category;
+        private int category;
         private string image;
         private float price;
         private int stock;
@@ -21,7 +22,7 @@ namespace JAHubLib
         public int Id { get => id; set => id = value; }
         public string Name { get => name; set => name = value; }
         public string Description { get => description; set => description = value; }
-        public string Category { get => category; set => category = value; }
+        public int Category { get => category; set => category = value; }
         public string Image { get => image; set => image = value; }
         public float Price { get => price; set => price = value; }
         
@@ -34,6 +35,52 @@ namespace JAHubLib
             string filename = $"product-{DateTime.Now.Ticks}.jpg";
 
             return Utilities.CopyImage(Image, filename); 
+        }
+
+        public void FetchReviews()
+        {
+            try
+            {
+                reviews.Clear();
+
+                using (SqlConnection connection = new SqlConnection(Utilities.getConnectionString()))
+                {
+                    connection.Open();
+
+                    string query = $"Select * from [Review] " +
+                        $"WHERE Product = {this.id}";
+
+                    SqlCommand cmd = new SqlCommand(query, connection);
+
+                    using (SqlDataReader sqlData = cmd.ExecuteReader())
+                    {
+                        
+
+                        while (sqlData.Read())
+                        {
+                            Review review = new Review();
+
+                            review.Id = (int)sqlData["ID"];
+                            review.Comment = sqlData["Comment"].ToString();
+                            review.Rating = (int)sqlData["Rating"];
+                            review.Customer = new Customer((int)sqlData["Customer"]);
+                           
+                            this.reviews.Add(review);
+                        }
+
+                        
+
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+           
         }
 
     }
