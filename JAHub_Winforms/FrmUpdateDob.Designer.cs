582fa35c1ad98116caba16aaedebc10f69860bc4
Garaine Baker <garainebaker01@gmail.com>
2022-04-05 18:15:38 -0500

    added updateDOB form

---
+++ JAHub_Winforms/FrmUpdateDob.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace JAHub_Winforms
+{
+    partial class FrmUpdateDob
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblBirthdayTlabel = new System.Windows.Forms.Label();
+            this.lblJAHAccontlabel1 = new System.Windows.Forms.Label();
+            this.btnBack = new FontAwesome.Sharp.IconButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtUpdateLastName = new System.Windows.Forms.TextBox();
+            this.lblUpdateDOBDaylabel = new System.Windows.Forms.Label();
+            this.btnLName = new System.Windows.Forms.Button();
+            this.lblUpdateDOBMonthlabel = new System.Windows.Forms.Label();
+            this.btnFName = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblUpdateDOBYearlable = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Controls.Add(this.lblJAHAccontlabel1);
+            this.panel1.Controls.Add(this.lblBirthdayTlabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 86);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblBirthdayTlabel
+            // 
+            this.lblBirthdayTlabel.AutoSize = true;
+            this.lblBirthdayTlabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBirthdayTlabel.Location = new System.Drawing.Point(105, 50);
+            this.lblBirthdayTlabel.Name = "lblBirthdayTlabel";
+            this.lblBirthdayTlabel.Size = new System.Drawing.Size(82, 25);
+            this.lblBirthdayTlabel.TabIndex = 0;
+            this.lblBirthdayTlabel.Text = "Birthbay";
+            // 
+            // lblJAHAccontlabel1
+            // 
+            this.lblJAHAccontlabel1.AutoSize = true;
+            this.lblJAHAccontlabel1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJAHAccontlabel1.Location = new System.Drawing.Point(10, 9);
+            this.lblJAHAccontlabel1.Name = "lblJAHAccontlabel1";
+            this.lblJAHAccontlabel1.Size = new System.Drawing.Size(118, 25);
+            this.lblJAHAccontlabel1.TabIndex = 4;
+            this.lblJAHAccontlabel1.Text = "JAH Account";
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Transparent;
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.ForeColor = System.Drawing.Color.Transparent;
+            this.btnBack.IconChar = FontAwesome.Sharp.IconChar.ArrowLeft;
+            this.btnBack.IconColor = System.Drawing.Color.Black;
+            this.btnBack.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnBack.IconSize = 30;
+            this.btnBack.Location = new System.Drawing.Point(47, 52);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(63, 27);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.lblUpdateDOBYearlable);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.btnCancel);
+            this.panel2.Controls.Add(this.txtUpdateLastName);
+            this.panel2.Controls.Add(this.lblUpdateDOBDaylabel);
+            this.panel2.Controls.Add(this.btnLName);
+            this.panel2.Controls.Add(this.lblUpdateDOBMonthlabel);
+            this.panel2.Controls.Add(this.btnFName);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(123, 142);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(501, 264);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Location = new System.Drawing.Point(383, 223);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(81, 25);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Location = new System.Drawing.Point(296, 223);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(81, 25);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // txtUpdateLastName
+            // 
+            this.txtUpdateLastName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUpdateLastName.Location = new System.Drawing.Point(181, 67);
+            this.txtUpdateLastName.Multiline = true;
+            this.txtUpdateLastName.Name = "txtUpdateLastName";
+            this.txtUpdateLastName.Size = new System.Drawing.Size(80, 33);
+            this.txtUpdateLastName.TabIndex = 7;
+            // 
+            // lblUpdateDOBDaylabel
+            // 
+            this.lblUpdateDOBDaylabel.AutoSize = true;
+            this.lblUpdateDOBDaylabel.Location = new System.Drawing.Point(187, 51);
+            this.lblUpdateDOBDaylabel.Name = "lblUpdateDOBDaylabel";
+            this.lblUpdateDOBDaylabel.Size = new System.Drawing.Size(26, 13);
+            this.lblUpdateDOBDaylabel.TabIndex = 6;
+            this.lblUpdateDOBDaylabel.Text = "Day";
+            // 
+            // btnLName
+            // 
+            this.btnLName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLName.Location = new System.Drawing.Point(179, 57);
+            this.btnLName.Name = "btnLName";
+            this.btnLName.Size = new System.Drawing.Size(95, 46);
+            this.btnLName.TabIndex = 5;
+            this.btnLName.UseVisualStyleBackColor = true;
+            // 
+            // lblUpdateDOBMonthlabel
+            // 
+            this.lblUpdateDOBMonthlabel.AutoSize = true;
+            this.lblUpdateDOBMonthlabel.Location = new System.Drawing.Point(41, 51);
+            this.lblUpdateDOBMonthlabel.Name = "lblUpdateDOBMonthlabel";
+            this.lblUpdateDOBMonthlabel.Size = new System.Drawing.Size(37, 13);
+            this.lblUpdateDOBMonthlabel.TabIndex = 3;
+            this.lblUpdateDOBMonthlabel.Text = "Month";
+            // 
+            // btnFName
+            // 
+            this.btnFName.BackColor = System.Drawing.Color.Transparent;
+            this.btnFName.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnFName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFName.Location = new System.Drawing.Point(33, 57);
+            this.btnFName.Name = "btnFName";
+            this.btnFName.Size = new System.Drawing.Size(117, 46);
+            this.btnFName.TabIndex = 2;
+            this.btnFName.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(31, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UPDATE BIRTHDAY";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(298, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 33);
+            this.textBox1.TabIndex = 11;
+            // 
+            // lblUpdateDOBYearlable
+            // 
+            this.lblUpdateDOBYearlable.AutoSize = true;
+            this.lblUpdateDOBYearlable.Location = new System.Drawing.Point(304, 51);
+            this.lblUpdateDOBYearlable.Name = "lblUpdateDOBYearlable";
+            this.lblUpdateDOBYearlable.Size = new System.Drawing.Size(29, 13);
+            this.lblUpdateDOBYearlable.TabIndex = 10;
+            this.lblUpdateDOBYearlable.Text = "Year";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(296, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 46);
+            this.button1.TabIndex = 9;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmUpdateDob
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmUpdateDob";
+            this.Text = "FrmUpdateDob";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblBirthdayTlabel;
+        private System.Windows.Forms.Label lblJAHAccontlabel1;
+        private FontAwesome.Sharp.IconButton btnBack;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtUpdateLastName;
+        private System.Windows.Forms.Label lblUpdateDOBDaylabel;
+        private System.Windows.Forms.Button btnLName;
+        private System.Windows.Forms.Label lblUpdateDOBMonthlabel;
+        private System.Windows.Forms.Button btnFName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblUpdateDOBYearlable;
+        private System.Windows.Forms.Button button1;
+    }
+}
