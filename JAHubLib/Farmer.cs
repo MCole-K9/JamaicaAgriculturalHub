f2eb4384531dd32a1d36392d2f22d15a234be1b8
khalid sheriff <khalalimsheriff@gmail.com>
2022-04-09 18:03:14 -0500

    finishing the queries for Farmer.WriteToDatabase() method

--- JAHubLib/Farmer.cs
+++ JAHubLib/Farmer.cs
@@ -160,14 +160,104 @@ namespace JAHubLib
         {
             // This depends on the radaregistrationphase
 
+            // K.S.: Note: this doesn't write the filepath of the IdPicture, because i'm purposefully going to scrap
+            // it if time starts to runs out. it's not a MS item and if it is, it's only my problem
+
             if (this.RadaRegistrationPhase == RadaRegistrationType.AwaitingVerification)
             {
                 using (SqlConnection connection = new SqlConnection(Utilities.getConnectionString()))
                 {
                     connection.Open();
 
-                    // write all of the queries here
+                    // First, change the Name (Mostly as a formality)
+                    String farmerUpdateUser = $"UPDATE [User] SET FirstName = '{this.FirstName}'," +
+                        $"MiddleName = '{this.MiddleName}', LastName = '{this.LastName}'" +
+                        $"WHERE ID = {Session.UserId};";
 
+                    // Then, update the farmer record with the new information
+                    String farmerUpdateFarmer = $"UPDATE [Farmer] SET BusinessEmail = {this.Email}, " +
+                        $"NumberOfEmployees = {this.NumberOfEmployees}, UsesHeavyMachinery = {this.UsesHeavyMachinery}" +
+                        $"TRN = {this.TaxRegistrationNumber}, DateOfBirth = {this.DateOfBirth}, RadaRegistrationStatus = {this.RadaRegistrationPhase}" +
+                        $"WHERE UserID = {Session.UserId};";
+
+                    // Necessary because every other table will need FarmerID
+                    String farmerSelectFarmer = $"SELECT FarmerID FROM [Farmer] " +
+                        $"WHERE UserID = {Session.UserId};";
+
+                    // Inserts all the products listed under ProductsTypicallyProduced
+                    String farmerInsertTypicalProduct = $"INSERT INTO [Farmer_ProducedProduct] " +
+                        " (FarmerID, ProductName) VALUES ";
+                    foreach(String product in ProductsTypicallyProduced)
+                    {
+                        farmerInsertTypicalProduct += $"({this.FarmerId}, '{product}')";
+
+                        // This checks to see if the product in the list is the last, and if so appends a semicolon,
+                        // else a comma
+                        if (ProductsTypicallyProduced.IndexOf(product) == ProductsTypicallyProduced.Count - 1)
+                        {
+                            farmerInsertTypicalProduct += ";";
+                        }
+                        else
+                        {
+                            farmerInsertTypicalProduct += ", ";
+                        }
+                    }
+
+                    // Inserts all the associated land values under this.OwnedLand
+                    String farmerInsertLandInformation = $"INSERT INTO [Farmer_land]" +
+                        $" (LandMeasurement, Town, PoBox, Parish, OwnerID) VALUES";
+                    foreach(LandInformation land in this.OwnedLand)
+                    {
+                        farmerInsertLandInformation += $"({land.LandMeasurement}, '{land.LandAddressTown}', " +
+                            $"'{land.LandAddressPoBox}', '{land.LandAddressParish}', {this.FarmerId})";
+
+                        if (OwnedLand.IndexOf(land) == OwnedLand.Count - 1)
+                        {
+                            farmerInsertLandInformation += ";";
+                        }
+                        else
+                        {
+                            farmerInsertLandInformation += ", ";
+                        }
+                    }
+
+                    // Inserts all of the associated Organizations in this.Organizations
+                    String farmerInsertOrganization = $"INSERT INTO [Farmer_Organization]" +
+                        $" (FarmerID, Organization) VALUES";
+                    foreach(String organization in Organizations)
+                    {
+                        farmerInsertOrganization += $"()";
+
+                        if (Organizations.IndexOf(organization) == Organizations.Count - 1)
+                        {
+                            farmerInsertOrganization += ";";
+                        }
+                        else
+                        {
+                            farmerInsertOrganization += ", ";
+                        }
+                    }
+
+                    // Inserts all of the associated phone numbers under this.PhoneNumbers
+                    String farmerInsertPhoneNumber = $"INSERT INTO [Farmer_PhoneNumber]" +
+                        $" (FarmerID, PhoneNumber) VALUES";
+                    foreach(String phoneNumber in PhoneNumbers)
+                    {
+                        farmerInsertPhoneNumber += $"({this.FarmerId}, '{phoneNumber}')";
+
+                        if(PhoneNumbers.IndexOf(phoneNumber) == PhoneNumbers.Count - 1)
+                        {
+                            farmerInsertPhoneNumber += ";";
+                        }
+                        else
+                        {
+                            farmerInsertPhoneNumber += ", ";
+                        }
+                    }
+
+                    // activating all of the queries in a list
+                                        
+                    
                     connection.Close();
                 }
 
@@ -181,15 +271,17 @@ namespace JAHubLib
 
                     // First, change the name
                     String command = $"UPDATE [User] SET FirstName = '{this.FirstName}'," +
-                        $"MiddleName = '{this.MiddleName}', LastName = '{this.LastName}' WHERE ID = {Session.UserId}, ";
+                        $"MiddleName = '{this.MiddleName}', LastName = '{this.LastName}' " +
+                        $"WHERE ID = {Session.UserId};";
 
                     SqlCommand addInformation = new SqlCommand(command, connection);
 
                     addInformation.ExecuteNonQuery();
 
                     // Next, add the TRN, DateOfBirth, and RadaRegistrationPhase
-                    addInformation.CommandText = $"UPDATE Farmer SET TRN = {this.TaxRegistrationNumber}, DateOFBirth = {this.DateOfBirth}" +
-                        $", RadaRegistrationPhase = {this.RadaRegistrationPhase}";
+                    addInformation.CommandText = $"UPDATE [Farmer] SET TRN = {this.TaxRegistrationNumber}, DateOFBirth = {this.DateOfBirth}" +
+                        $", RadaRegistrationPhase = {this.RadaRegistrationPhase}" +
+                        $"WHERE UserID = {Session.UserId};";
                     addInformation.ExecuteNonQuery();
 
                     connection.Close();
