4cc51b6cfcfab2180ea3add183cac733044ffe9c
Svengalist <elliotmorrison58@gmail.com>
2022-04-11 15:32:12 -0500

    Created Blog Page

---
+++ JAHub_ASPWebforms/Blog.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using JAHubLib;
+
+namespace JAHub_ASPWebforms
+{
+	public partial class WebFrmBlog : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+            SqlConnection conn = new SqlConnection(Utilities.getConnectionString());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * FROM [Blog]", conn);
+            SqlDataReader sqlData = cmd.ExecuteReader();
+            int IDinc = 0;
+            while (sqlData.Read())
+            {
+                IDinc++;
+                Blog blog = new Blog();
+
+                blog.BlogID = (int)sqlData["ID"];
+                blog.Title = sqlData["Title"].ToString();
+                blog.AuthorID = (int)sqlData["Author"];
+                blog.Description = sqlData["Description"].ToString();
+                blog.BlogBody = sqlData["Body"].ToString();
+                blog.PublishDateString = sqlData["PublishedDate"].ToString();
+                blog.Rating = Convert.ToInt16(sqlData["Rating"]);
+                ucWebBlog ucWeb = (ucWebBlog)Page.LoadControl("~/ucWebBlog.ascx");
+                ucWeb.GetBlog(blog);
+                ucWeb.ID = "BlogControl" + IDinc.ToString();
+                pnlBlogContainer.Controls.Add(ucWeb);
+
+            }
+            conn.Close();
+		}
+		
+    }
+}
---
+++ JAHub_ASPWebforms/ucWebBlog.ascx
@@ -0,0 +1,36 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ucWebBlog.ascx.cs" Inherits="JAHub_ASPWebforms.ucWebBlog" %>
+<asp:Panel ID="pnlBlogControl" runat="server" BackColor="LightSeaGreen" BorderStyle="Outset" Height="100px" CssClass="pad">
+    <asp:Button ID="btnView" runat="server" Text="View" CssClass="ViewButton" Width="62px" OnClick="btnView_Click" />
+    <ul id="ulListWrapper">
+  <li id="liTitle">Title: 
+      <asp:Label ID="lblTitle" runat="server" Text="Label"></asp:Label>
+        </li>
+  <li id="liAuthor">Author: 
+      <asp:Label ID="lblAuthor" runat="server" Text="Label"></asp:Label>
+        </li>
+  <li id="liPublishDate">Date Published: 
+      <asp:Label ID="lblDatePublished" runat="server" Text="Label"></asp:Label>
+        </li>
+  <li id="liDescription">Description: 
+      <asp:Label ID="lblDescription" runat="server" Text="Label"></asp:Label>
+        </li>
+</ul>
+</asp:Panel>
+<style>
+    .pad{
+        padding:10px;
+        margin-bottom:5px;
+        }
+    ul{
+        list-style: none;
+    }
+    .ViewButton{
+      float:right;
+        height: 26px;
+    }
+    .blog-text-wrapper{
+        display:flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+</style>
