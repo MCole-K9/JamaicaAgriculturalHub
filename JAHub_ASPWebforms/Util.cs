4e4dd06a943faee534b558eae80091cde52f6dbe
Matthew Cole <m.b.cole300@gmail.com>
2022-04-11 16:23:38 -0500

    Created Util Class for project

---
+++ JAHub_ASPWebforms/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JAHub_ASPWebforms
+{
+    public static class Util
+    {
+        public static string GetSolFilePath()
+        {
+            string path = HttpRuntime.AppDomainAppPath;
+            return path.Substring(0, path.Length - 19);
+        }
+    }
+}
