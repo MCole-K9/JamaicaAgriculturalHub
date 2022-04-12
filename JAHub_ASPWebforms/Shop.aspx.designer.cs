5441c312d169798d80acdc8ce32b7f676debf327
Matthew Cole <m.b.cole300@gmail.com>
2022-04-12 10:44:22 -0500

    Panel Added

--- JAHub_ASPWebforms/Shop.aspx.designer.cs
+++ JAHub_ASPWebforms/Shop.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace JAHub_ASPWebforms
 
     public partial class Shop
     {
+
+        /// <summary>
+        /// pnlProducts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlProducts;
     }
 }
