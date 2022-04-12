34a16f42c155b9ab016918ac5eb0426e3f613389
Matthew Cole <m.b.cole300@gmail.com>
2022-04-12 10:40:20 -0500

    Created Product User Control(incomplete)

---
+++ JAHub_ASPWebforms/Shop_Controls/UsrProduct.ascx
@@ -0,0 +1,10 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UsrProduct.ascx.cs" Inherits="JAHub_ASPWebforms.Shop_Controls.UsrProduct" %>
+
+<div style="display:flex; justify-content:center; padding: 10px; height: 100px">
+   <div class="thumbnail">
+       <asp:Image ID="ProductImage" CssClass="img-responsive img-rounded" runat="server" />
+   </div>
+    <div class="conte">
+
+    </div>
+</div>
---
+++ JAHub_ASPWebforms/Shop_Controls/UsrProduct.ascx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using JAHubLib;
+
+namespace JAHub_ASPWebforms.Shop_Controls
+{
+    public partial class UsrProduct : System.Web.UI.UserControl
+    {
+        private Product product;
+        private int ImageIDSequence;
+        public void SetProduct(Product product)
+        {
+            this.product = product;
+        }
+        public void SetImageIDSequence(int ID)
+        {
+            this.ImageIDSequence = ID;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProductImage.ID += ID;
+
+            ProductImage.ImageUrl =$"~/{product.Image}";
+            
+    
+
+            
+        }
+    }
+}
