6f4fa1d5dfebf8046314ae8f9ef94d4ddee337b2
khalid sheriff <khalalimsheriff@gmail.com>
2022-04-15 01:19:11 -0500

    Added RequiredFieldValidators for textboxes is usrNameBlock

--- JAHub_ASPWebforms/Verification/usrNameBlock.ascx.cs
+++ JAHub_ASPWebforms/Verification/usrNameBlock.ascx.cs
@@ -9,14 +9,24 @@ namespace JAHub_ASPWebforms.Verification
 {
     public partial class usrNameBlock : System.Web.UI.UserControl
     {
-        // Need to add in properties from other control
+        // [x] Need to add in properties from other control
         // Need to figure out how to get variables
         // also need to change the format so that it doesn't look so bloody weird
         // why is the css not showing?
+        // how do i do validation?
+
+        bool isFirstNameValid;
+        bool isMiddleNameValid;
+        bool isLastNameValid;
+
+        public String FirstName => txtFirstName.Text;
+        public String MiddleName => txtMiddleName.Text;
+        public String LastName => txtLastName.Text;
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // This needs to distinguish between when parameters are set and when it's 
         }
     }
 }
