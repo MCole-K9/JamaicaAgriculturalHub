b00319d80fd3334bca2b163f979f9ef14f728a1b
Matthew Cole <m.b.cole300@gmail.com>
2022-04-15 09:14:10 -0500

    added id sequence for view btn and changed the view btn event click declaration

--- JAHub_ASPWebforms/Shop_Controls/UsrProduct.ascx.cs
+++ JAHub_ASPWebforms/Shop_Controls/UsrProduct.ascx.cs
@@ -25,6 +25,7 @@ namespace JAHub_ASPWebforms.Shop_Controls
             ProductImage.ID += IDSequence;
             ProductName.ID += IDSequence;
             ProductPrice.ID += IDSequence;
+            btnViewProduct.ID += IDSequence;
 
             ProductImage.Src =$"~/{product.Image}";
 
@@ -34,7 +35,7 @@ namespace JAHub_ASPWebforms.Shop_Controls
             
         }
 
-        protected void btnViewProduct_Click(object sender, EventArgs e)
+        protected void btnViewProduct_ServerClick(object sender, EventArgs e)
         {
             Session["ProductDetail"] = this.product;
             Response.Redirect("~/ProductDetail.aspx");
