069f010825a769a9cedcd282b4cd52e648b709f8
Matthew Cole <m.b.cole300@gmail.com>
2022-04-17 11:39:38 -0500

    Created Static Class for Cart

---
+++ JAHubLib/Cart.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAHubLib
+{
+    public static class Cart
+    {
+        public static Dictionary<Product, int> ShoppingCart = new Dictionary<Product, int>();
+
+        public static void AddToCart(Product product, int quantity)
+        {
+            try
+            {
+                ShoppingCart.Add(product, quantity);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static void UpdateProductQuantity(Product product, int quantity)
+        {
+            ShoppingCart[product] = quantity;
+        }
+
+        public static void RemoveFromCart(Product product)
+        {
+            ShoppingCart.Remove(product);
+        }
+
+        public static List<Product> GetProductsInCart()
+        {
+            return ShoppingCart.Keys.ToList();
+        }
+
+        public static float CaluculateTotal()
+        {
+            float total = 0.0f;
+
+            foreach (Product product in ShoppingCart.Keys.ToList())
+            {
+                total += product.Price * ShoppingCart[product];
+            }
+            return total;
+        }
+
+
+        public static float CostOfProductOrder(Product product)
+        {
+            return product.Price * ShoppingCart[product];
+        }
+    }
+}
+    
