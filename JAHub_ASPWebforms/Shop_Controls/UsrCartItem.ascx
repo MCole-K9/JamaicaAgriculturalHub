b325af01d4222f9f645218d174f76693f8ff5d49
Matthew Cole <m.b.cole300@gmail.com>
2022-04-18 00:53:28 -0500

    Created CartItem User Control

---
+++ JAHub_ASPWebforms/Shop_Controls/UsrCartItem.ascx
@@ -0,0 +1,27 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UsrCartItem.ascx.cs" Inherits="JAHub_ASPWebforms.Shop_Controls.UsrCartItem" %>
+
+<div class="row">
+    <div class="col-xs-3 col-height">
+        <img id="ProductImage" runat="server" class="img-responsive img-thumbnail" src="#">
+    </div>
+    <div class="col-xs-4 col-height">
+        <h4 runat="server" id="lblProductName">Product name</h4>
+    </div>
+    <div class="col-xs-5 col-height">
+        <div class="col-xs-4 text-right col-height">
+            <h6>$<strong><span id="Price" runat="server">25.00</span> <span class="text-muted">x</span></strong></h6>
+        </div>
+        <div class="col-xs-6 col-height">
+            <div style="display: flex; justify-content: center; align-items: center">
+                <asp:TextBox CssClass="qty-input form-control input-sm " AutoPostBack="true" OnTextChanged="txtQuantity_TextChanged" ID="txtQuantity"  runat="server" Text="1"></asp:TextBox>
+                <%--<input change runat="server" style="flex-grow: 1; margin-right: 5px;" type="text" class="form-control input-sm" value="1">--%>
+                <asp:Label ID="lblUnit" runat="server" Text="Lbs"></asp:Label>
+            </div>
+        </div>
+        <div class="col-xs-2 col-height">
+            <button type="button" class="btn btn-link btn-xs btn-style">
+                <span class="glyphicon glyphicon-trash"></span>
+            </button>
+        </div>
+    </div>
+</div>
---
+++ JAHub_ASPWebforms/Shop_Controls/UsrCartItem.ascx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using JAHubLib;
+
+namespace JAHub_ASPWebforms.Shop_Controls
+{
+    public partial class UsrCartItem : System.Web.UI.UserControl
+    {
+        Product product;
+        int IDSequence;
+        public void SetProduct(Product product)
+        {
+            this.product = product;
+        }
+        public void SetIDSequence(int id)
+        {
+            this.IDSequence = id;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+
+                PopulateFields();
+                Price.ID += IDSequence;
+                ProductImage.ID += IDSequence;
+                lblProductName.ID += IDSequence;
+                txtQuantity.Text = JAHubLib.Cart.ShoppingCart[product].ToString();
+
+            }
+        }
+
+        private void PopulateFields()
+        {
+            if(product != null)
+            {
+                Price.InnerText = product.Price.ToString();
+                ProductImage.Src = product.Image;
+                lblProductName.InnerText = product.Name;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        protected void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+            float qty;
+            float.TryParse(txtQuantity.Text, out qty);
+
+            if(qty > 0)
+            {
+                JAHubLib.Cart.UpdateProductQuantity(this.product, (int)qty);
+
+            }
+        }
+    }
+}
