704df9f2d7208881fee4232025245219e20954fb
khalid sheriff <khalalimsheriff@gmail.com>
2022-04-20 01:52:54 -0500

    created space for Moderation usercontrols; added usrAddInfraction and usrInfractionItem (components, no code)

---
+++ JAHub_ASPWebforms/Moderation/usrAddInfraction.ascx
@@ -0,0 +1,8 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="usrAddInfraction.ascx.cs" Inherits="JAHub_ASPWebforms.Moderation.usrAddInfraction" %>
+
+<div>
+    <asp:Label runat="server"></asp:Label>
+    <asp:TextBox runat="server"></asp:TextBox>
+    <asp:Button runat="server"/>
+
+</div>
---
+++ JAHub_ASPWebforms/Moderation/usrInfractionItem.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JAHub_ASPWebforms.Moderation
+{
+    public partial class usrViewInfraction : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
