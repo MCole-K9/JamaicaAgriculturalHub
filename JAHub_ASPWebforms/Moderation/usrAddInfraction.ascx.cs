d15bdd25114b8d6ad438ccf5d30b0b4502d6ef17
khalid sheriff <khalalimsheriff@gmail.com>
2022-04-20 02:43:37 -0500

    wrote .cs for usrAddInfraction.ascx

--- JAHub_ASPWebforms/Moderation/usrAddInfraction.ascx.cs
+++ JAHub_ASPWebforms/Moderation/usrAddInfraction.ascx.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.SqlTypes;
 
 namespace JAHub_ASPWebforms.Moderation
 {
     public partial class usrAddInfraction : System.Web.UI.UserControl
     {
+        DataTable addedInfractions;
+        SqlDateTime currentTime;
+        int userId;
+        int adminId;
+        PlaceHolder controlParent;
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            currentTime = new SqlDateTime(DateTime.Now);
+            lblTimeStamp.Text = currentTime.ToString();
+        }
 
+        
+
+        public usrAddInfraction (DataTable addedInfractions, int userId, int adminId, PlaceHolder controlParent)
+        {
+            this.userId = userId;
+            this.adminId = adminId;
+            this.addedInfractions = addedInfractions;
+            this.controlParent = controlParent;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // This should add the the generated infraction information to dtbAddedInfractions
+            // it should also remove this control and create a new usrInfractionItem
+            //addedInfractions.Rows.Add(currentTime, userId, adminId, txtInfractionReason.Text);
+
+            //controlParent.Controls.Add(new usrInfractionItem(controlParent, addedInfractions.Rows[addedInfractions.Rows.Count - 1]));
+            //controlParent.Controls.Remove(this);
         }
     }
 }
