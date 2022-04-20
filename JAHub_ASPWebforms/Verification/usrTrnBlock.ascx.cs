1a9a6ec771435579a6f6c9df67e5352368282172
khalid sheriff <khalalimsheriff@gmail.com>
2022-04-20 00:58:45 -0500

    created and filled in usrTrnBlock

---
+++ JAHub_ASPWebforms/Verification/usrTrnBlock.ascx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JAHub_ASPWebforms.Verification
+{
+    public partial class usrTrnBlock : System.Web.UI.UserControl
+    {
+        public String TaxRegistrationNumber => txtTaxRegistrationNumber.Text;
+
+        public bool isTrnValid = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public usrTrnBlock()
+        {
+
+        }
+
+        public usrTrnBlock(String trn)
+        {
+            this.txtTaxRegistrationNumber.Text = trn;
+
+            isTrnValid = true;
+        }
+
+        protected void cusTaxRegistrationNumber_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if(!Int32.TryParse(txtTaxRegistrationNumber.Text, out int taxRegistrationNumber))
+            {
+                isTrnValid = false;
+            }
+        }
+    }
+}
