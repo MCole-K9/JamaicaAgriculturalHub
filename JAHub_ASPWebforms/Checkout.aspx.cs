4ff21e9fd95e4c0bb073b5d30e489a47c2edc7a7
Matthew Cole <m.b.cole300@gmail.com>
2022-05-01 00:26:50 -0500

    Redirecting To Order Details after Sucessful Order

--- JAHub_ASPWebforms/Checkout.aspx.cs
+++ JAHub_ASPWebforms/Checkout.aspx.cs
@@ -67,6 +67,8 @@ namespace JAHub_ASPWebforms
                 if (order.WriteOrderItems(JAHubLib.Cart.ShoppingCart) > 0)
                 {
                     //MessageBox.Show("Order Was Successful");
+                    Session["LastOrder"] = order;
+                    Response.Redirect("OrderDetails.aspx");
                 }
                 else
                 {
