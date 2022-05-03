09fe999fd6cf40b32a3537d0fcefb800f724c6fb
Matthew Cole <m.b.cole300@gmail.com>
2022-05-03 08:13:09 -0500

    Added Order Item User Control

---
+++ JAHub_ASPWebforms/Shop_Controls/UsrOrder.ascx
@@ -0,0 +1,21 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UsrOrder.ascx.cs" Inherits="JAHub_ASPWebforms.Shop_Controls.UsrOrder" %>
+
+<div class="panel">
+    <div class="page-header" style="display: flex; justify-content: space-between;">
+        <div>
+            <h5 class="text-muted"><strong>Shipping To</strong> </h5>
+            <h5>Street Adrees: <span id="ShipStreetAdress" runat="server"></span></h5>
+            <h5>City: <span id="ShipCity" runat="server"></span></h5>
+            <h5>Parish: <span id="ShipParish" runat="server"></span></h5>
+        </div>
+        <div>
+            <h5 class="text-muted"><strong>Ordered On</strong> </h5>
+             <h5 id="OrderDate" runat="server">Date</h5>
+        </div>
+        <div>
+            <h5 class="text-muted"><strong>Subtotal</strong> </h5>
+             <h5 id="Subtotal" runat="server">Date</h5>
+        </div>
+        <button class="btn btn-default btn-link fit-content-x">View Order Details</button>
+    </div>
+</div>
---
+++ JAHub_ASPWebforms/Shop_Controls/UsrOrder.ascx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using JAHubLib;
+
+namespace JAHub_ASPWebforms.Shop_Controls
+{
+    public partial class UsrOrder : System.Web.UI.UserControl
+    {
+        private Order order;
+        
+        int IDSequence;
+
+        public void SetOrder(Order order)
+        {
+            this.order = order;
+        }
+        public void SetIDSequence(int id)
+        {
+            this.IDSequence = id;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ShipStreetAdress.InnerText = order.ShipStreetAddress;
+                ShipCity.InnerText = order.ShipCity;
+                ShipParish.InnerText = order.ShipParish;
+
+                OrderDate.InnerText = order.OrderDate.ToString("dddd, MMMM dd, yyyy");
+                Subtotal.InnerText = $"${order.TotalAmount}";
+            }
+           
+        }
+    }
+}
