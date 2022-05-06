fa565a3112df6f7f56caaca1451ee95cba897d9c
khalid sheriff <khalalimsheriff@gmail.com>
2022-05-06 03:21:24 -0500

    wrote database code for RadaRegistration; Modified FrmRadaRegister to reinstate SetFocus; began writing code to pull and show information from database

--- JAHub_ASPWebforms/RadaRegistration.aspx.cs
+++ JAHub_ASPWebforms/RadaRegistration.aspx.cs
@@ -6,6 +6,8 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using JAHubLib;
 using JAHub_ASPWebforms.Verification;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace JAHub_ASPWebforms
 {
@@ -13,6 +15,7 @@ namespace JAHub_ASPWebforms
     {
         protected bool fullRegistration;
 
+        usrNameBlock nameBlock;
         usrAddressBlock addressBlock;
         usrContactBlock contactBlock;
         usrDateOfBirthBlock dateOfBirthBlock;
@@ -24,14 +27,12 @@ namespace JAHub_ASPWebforms
         {
             if (PreviousPage.FarmerRegistrationPhase == RadaRegistrationType.NotRegistered)
             {
-                // Create all of the controls for entry and let them see them
+                // 
 
             }
             else if (PreviousPage.FarmerRegistrationPhase == RadaRegistrationType.FullyConnected)
             {
-                lblRadaRegistrationHeading.Text = "Edit Information";
-                // hide the button to choose between the short and long forms of doing the thing
-                // Either hide the submit button or choose edit or something, idk
+
             }
         }
 
@@ -53,12 +54,18 @@ namespace JAHub_ASPWebforms
             else if (PreviousPage.FarmerRegistrationPhase == RadaRegistrationType.FullyConnected)
             {
                 lblRadaRegistrationHeading.Text = "Edit/View Information";
-                btnSubmit.Visible = false;
+                btnSubmit.Text = "Update Record";
                 FindControl("divAccountCreation").Visible = false;
 
                 GenerateFullRegistration();
 
-                // query the entire farmer thing and write it out
+                if (!IsPostBack)
+                {
+                    Farmer farmer = new Farmer();
+                    farmer.GetFullRecordFromDatabase((int)Session["UserId"]);
+
+
+                }
                 // what if a postback happens?
             }
         }
@@ -79,40 +86,119 @@ namespace JAHub_ASPWebforms
 
         protected void GenerateFullRegistration()
         {
+            nameBlock = (usrNameBlock)LoadControl("~/Verification/usrNameBlock.ascx");
             addressBlock = (usrAddressBlock)LoadControl("~/Verification/usrAddressBlock.ascx");
             contactBlock = (usrContactBlock)LoadControl("~/Verification/usrContactBlock.ascx");
             dateOfBirthBlock = (usrDateOfBirthBlock)LoadControl("~/Verificaton/usrDateOfBirthBlock.ascx");
             holdingsBlock = (usrHoldingsBlock)LoadControl("~/Verificaton/usrHoldingsBlock.ascx");
             trnBlock = (usrTrnBlock)LoadControl("~/Verification/usrTrnBlock.ascx");
             industryBlock = (usrIndustryBlock)LoadControl("~/Verificaton/usrIndustryBlock.ascx");
-            organizationsBlock = (usrOrganizationsBlock)LoadControl("~/Verification/usrOrganizationzBlock.ascx");
+            organizationsBlock = (usrOrganizationsBlock)LoadControl("~/Verification/usrOrganizationsBlock.ascx");
+
+            phRadaRegistration.Controls.Add(nameBlock);
+            phRadaRegistration.Controls.Add(dateOfBirthBlock);
+            phRadaRegistration.Controls.Add(contactBlock);
+            phRadaRegistration.Controls.Add(trnBlock);
+            phRadaRegistration.Controls.Add(industryBlock);
+            phRadaRegistration.Controls.Add(organizationsBlock);
+            phRadaRegistration.Controls.Add(holdingsBlock);
         }
 
         protected void GeneratePartialRegistration()
         {
-            addressBlock = (usrAddressBlock)LoadControl("~/Verification/usrAddressBlock.ascx");
+            nameBlock = (usrNameBlock)LoadControl("~/Verification/usrNameBlock.ascx");
+            //addressBlock = (usrAddressBlock)LoadControl("~/Verification/usrAddressBlock.ascx");
             contactBlock = (usrContactBlock)LoadControl("~/Verification/usrContactBlock.ascx");
             dateOfBirthBlock = (usrDateOfBirthBlock)LoadControl("~/Verificaton/usrDateOfBirthBlock.ascx");
-            holdingsBlock = (usrHoldingsBlock)LoadControl("~/Verificaton/usrHoldingsBlock.ascx");
+            //holdingsBlock = (usrHoldingsBlock)LoadControl("~/Verificaton/usrHoldingsBlock.ascx");
             trnBlock = (usrTrnBlock)LoadControl("~/Verification/usrTrnBlock.ascx");
-            industryBlock = (usrIndustryBlock)LoadControl("~/Verificaton/usrIndustryBlock.ascx");
-            organizationsBlock = (usrOrganizationsBlock)LoadControl("~/Verification/usrOrganizationzBlock.ascx");
+            //industryBlock = (usrIndustryBlock)LoadControl("~/Verificaton/usrIndustryBlock.ascx");
+            //organizationsBlock = (usrOrganizationsBlock)LoadControl("~/Verification/usrOrganizationzBlock.ascx");
+
+            phRadaRegistration.Controls.Add(nameBlock);
+            phRadaRegistration.Controls.Add(dateOfBirthBlock);
+            phRadaRegistration.Controls.Add(trnBlock);
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
             Page.Validate();
 
-            // change customer to farmer if they're so and so.
+            if (Page.IsValid)
+            {
+                // change customer to farmer if valid
+                if ((UserRole)Session["UserRole"] == UserRole.Customer)
+                {
+                    using (SqlConnection connection = new SqlConnection(Utilities.getConnectionString()))
+                    {
+                        connection.Open();
 
-            if (fullRegistration)
-            {
-                // do the full submission for blah blah
+                        // First: Change the UserRole in the User Table
+                        String command = $"UPDATE [User] SET UserRole = 3 WHERE ID = {Session["UserId"]}";
+
+                        SqlCommand changeCustomerToFarmer = new SqlCommand(command, connection);
+
+                        changeCustomerToFarmer.ExecuteNonQuery();
+
+                        // Then: Create a new record for the Farmer-to-be using their UserID
+                        changeCustomerToFarmer.CommandText = $"INSERT INTO [Farmer] (UserId) VALUES ({Session["UserId"]})";
+
+                        changeCustomerToFarmer.ExecuteNonQuery();
+
+                        connection.Close();
+                    }
+                }
+
+
+                if (fullRegistration)
+                {
+                    Farmer newFarmer = new Farmer();
+
+                    newFarmer.FirstName = nameBlock.FirstName;
+                    newFarmer.MiddleName = nameBlock.MiddleName;
+                    newFarmer.LastName = nameBlock.LastName;
+
+                    newFarmer.TaxRegistrationNumber = trnBlock.TaxRegistrationNumber;
+
+                    newFarmer.DateOfBirth = new System.Data.SqlTypes.SqlDateTime(dateOfBirthBlock.Year, dateOfBirthBlock.Month, dateOfBirthBlock.Day);
+
+                    newFarmer.BusinessEmail = contactBlock.Email;
+                    newFarmer.PhoneNumbers = contactBlock.PhoneNumbers;
+
+                    newFarmer.ProductsTypicallyProduced = holdingsBlock.ProductList;
+                    newFarmer.OwnedLand = holdingsBlock.LandInformation;
+
+                    newFarmer.Organizations = organizationsBlock.Organizations;
+
+                    newFarmer.UsesHeavyMachinery = industryBlock.UsesHeavyMachinery;
+                    newFarmer.NumberOfEmployees = Decimal.ToInt32(industryBlock.NumberOfEmployees);
+
+                    newFarmer.RadaRegistrationPhase = RadaRegistrationType.AwaitingVerification;
+
+                    newFarmer.WriteRecordToDatabase((int)Session["UserId"]);
+
+                    Response.Redirect("~/RadaStatus.aspx");
+                }
+                else
+                {
+                    Farmer newFarmer = new Farmer();
+
+                    newFarmer.FirstName = nameBlock.FirstName;
+                    newFarmer.MiddleName = nameBlock.MiddleName;
+                    newFarmer.LastName = nameBlock.LastName;
+
+                    newFarmer.TaxRegistrationNumber = trnBlock.TaxRegistrationNumber;
+
+                    newFarmer.DateOfBirth = new System.Data.SqlTypes.SqlDateTime(dateOfBirthBlock.Year, dateOfBirthBlock.Month, dateOfBirthBlock.Day);
+
+                    newFarmer.RadaRegistrationPhase = RadaRegistrationType.NotConnected;
+
+                    newFarmer.WriteRecordToDatabase((int)Session["UserId"]);
+
+                    Response.Redirect("~/RadaStatus.aspx");
+                }
             }
-            else
-            {
-                // do the partial submission
-            }
+            
         }
     }
 }
